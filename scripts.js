1576cde4609aa81eeeeda4cff84b9b71f07bf128
s0604798 <juri.wende@student.htw-berlin.de>
2026-08-06 18:23:08 +0200

    add sample project

--- scripts.js
+++ scripts.js
@@ -1,7 +1,11 @@
 let expandables = document.querySelectorAll('.expandable');
 
-let project_nav = expandables[0];
-let projects = document.querySelector('#projects');
-project_nav.addEventListener('click', function () {
-    projects.classList.toggle('hide');
+let projects_container = expandables[0];
+let icon = projects_container.children[0];
+let personal_projects = projects_container.children[1];
+let projects_nav = projects_container.children[2];
+
+personal_projects.addEventListener('click', function () {
+    icon.classList.toggle('open');
+    projects_nav.classList.toggle('hide');
 });

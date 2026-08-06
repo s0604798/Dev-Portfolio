fed90188459993b101ed48430465617dae9ea851
s0604798 <juri.wende@student.htw-berlin.de>
2026-08-06 19:11:06 +0200

    main display

--- scripts.js
+++ scripts.js
@@ -1,5 +1,23 @@
-let expandables = document.querySelectorAll('.expandable');
+//##################################################// MAIN DISPLAY
+const display = document.querySelector('#display');
+console.log(display);
 
+//##################################################// MAIN NAV
+const pages = document.querySelectorAll('.page');
+
+for (let page of pages) {
+    let src = page.dataset.src;
+
+    console.log(page);
+    console.log(src);
+
+    page.addEventListener('click', function () {
+        display.src = src;
+    })
+}
+
+//##################################################// PROJECT NAV
+let expandables = document.querySelectorAll('.expandable');
 let projects_container = expandables[0];
 let icon = projects_container.children[0];
 let personal_projects = projects_container.children[1];

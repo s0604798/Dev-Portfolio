24a1c28da701e9b22bb569796b36f455a1af4523
Juri Wende <juri.wende@proton.me>
2026-08-16 22:47:38 +0200

    screwwww uuuu

--- scripts.js
+++ scripts.js
@@ -1,29 +1,76 @@
 //##################################################// MAIN DISPLAY
+
 const display = document.querySelector('#display');
-console.log(display);
+let currentPage = document.querySelector('#p-1');
+
+const observer = new IntersectionObserver((entries) => {
+
+    console.log('entries:');
+    console.log(entries);
+    entries.forEach((entry) => {
+        console.log('entry target:');
+        console.log(entry.target);
+        if (entry.isIntersecting) {
+            console.log('element within view');
+        } else {
+            console.log('element is out of view!');
+        }
+    });
+}, { root: display, rootMargin: '10px', threshold: 0.1 });
+
+let page = {
+    displayID: 'p-1',
+    displayElement: document.querySelector('#p-1'),
+
+    navID: 'n-1',
+    navElement: document.querySelector('#n-1'),
+}
+
+let timeout;
+let ms = 150;
+let lastScroll = display.scrollTop;
+
+display.addEventListener('scroll', function (event) {
+    if (timeout) clearTimeout(timeout);
+    timeout = setTimeout(() => {
+        timeout = null;
+
+        //logic
+        let currentScroll = display.scrollTop;
+
+        if (currentScroll < lastScroll) {
+            console.log('up');
+
+            //currentPage = currentPage.previousElementSibling;
+
+        } else if (currentScroll > lastScroll) {
+            console.log('down');
+
+            //currentPage = currentPage.nextElementSibling;
+        }
+        lastScroll = currentScroll;
+    }, ms);
+});
+
+display.addEventListener('scrollend', function (event) {
+    console.log('current page: ');
+    console.log(currentPage);
+    observer.observe(currentPage);
+});
 
 //##################################################// MAIN NAV
-const pages = document.querySelectorAll('.page');
+const navPages = document.querySelectorAll('.navPage');
+for (let navPage of navPages) {
 
-for (let page of pages) {
-    let src = page.dataset.src;
+    navPage.addEventListener('click', function (event) {
+        const pageID = 'p-' + navPage.id.match(/[0-9]/g)[0];
+        const nextPage = document.getElementById(pageID);
 
-    console.log(page);
-    console.log(src);
-
-    page.addEventListener('click', function () {
-        display.src = src;
-    })
+        currentPage = nextPage;
+        nextPage.scrollIntoView(true);
+    });
 }
 
 //##################################################// PROJECT NAV
-let expandables = document.querySelectorAll('.expandable');
-let projects_container = expandables[0];
-let icon = projects_container.children[0];
-let personal_projects = projects_container.children[1];
-let projects_nav = projects_container.children[2];
-
-personal_projects.addEventListener('click', function () {
-    icon.classList.toggle('open');
-    projects_nav.classList.toggle('hide');
-});
+const projects_container = document.querySelector('.expandable').children;
+const arrow = projects_container[0]; const projects = projects_container[2];
